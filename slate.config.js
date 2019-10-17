e8e951248c66a7f5e9ba5744d68ace23712bb4f3
Daniel Hunninghake <dhunn1587@gmail.com>
2019-10-17 17:57:59 -0400

    Update deps

---
+++ slate.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+// Configuration file for all things Slate.
+// For more information, visit https://github.com/Shopify/slate/wiki/Slate-Configuration
+
+const path = require('path');
+
+module.exports = {
+  'webpack.extend': {
+    resolve: {
+      alias: {
+        jquery: path.resolve('./node_modules/jquery')
+      }
+    }
+  }
+};
